c961a5bfdc107272c9f8560ddf669bce964487a0
vinsol <vinsol@vinsols-MacBook-Pro.local>
2023-10-21 21:57:17 +0530

    added pagination

--- src/components/list.js
+++ src/components/list.js
@@ -1,21 +1,49 @@
 import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 
 import getPlayers from "../apis/get-players.js";
 
+const PER_PAGE = 10;
+
 const CricketersList = () => {
   const [list, setList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-
     const fetchData = async () => {
       const data = await getPlayers();
-      setList(data)
+      console.log(data, "data");
+      setList(data);
     };
-    
+
     fetchData();
   }, []);
 
-  return <div>Cricker</div>;
+  const offset = currentPage * PER_PAGE;
+
+  const filteredList = list.slice(offset, offset + PER_PAGE);
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage)
+  }
+
+  const pageCount = Math.ceil(list.length / PER_PAGE);
+
+  return (
+    <div>
+      <div>
+        {filteredList.map(({ name }) => (
+          <div>{name}</div>
+        ))}
+      </div>
+      <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+      />
+    </div>
+  );
 };
 
 export default CricketersList;
